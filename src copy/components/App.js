74810ab5df5f6b4d6e1930d422229d9e039362e1
colin <56652916+ilgen@users.noreply.github.com>
2019-10-16 12:17:10 -0700

    Add files via upload
    source code for evens-or-odds react/redux application

---
+++ src copy/components/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { startGame, cancelGame } from '../actions/settings';
+
+class App extends Component {
+
+    render() {
+        console.log('this', this);
+
+        return (
+            <div>
+                <h2>♠ ♥ Evens 🔥 Odds ♦ ♣</h2>
+                {
+                    this.props.gameStarted ? (
+                        <div>
+                            <h3>The game is on!</h3>
+                            <br />
+                            <button onClick={this.props.cancelGame}>Cancel Game</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <h3>A new game awaits</h3>
+                            <br />
+                            <button onClick={this.props.startGame}>Start Game</button>
+                        </div>
+                    )
+                }            
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    console.log('state', state);
+
+    return { gameStarted: state.gameStarted };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        startGame: () => dispatch(startGame()), 
+        cancelGame: () => dispatch(cancelGame())
+    };
+}
+
+const componentConnector = connect(mapStateToProps, mapDispatchToProps);
+
+export default componentConnector(App);
