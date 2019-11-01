2fb390b73a6683314bec4a73dd3c6d3b6a4d93a0
colin <56652916+ilgen@users.noreply.github.com>
2019-10-31 19:20:25 -0700

    Add files via upload

---
+++ src/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import PubSub, { PubSubContext } from './pubsub';
+import App from './components/App';
+import { newMessage } from './actions/messages';
+import './index.css';
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
+console.log('store.getState()', store.getState());
+store.subscribe(() => console.log('store.getState()', store.getState()));
+
+const pubsub = new PubSub();
+
+pubsub.addListener({
+    message: messageObject => {
+        const { message, channel } = messageObject;
+
+        console.log('Received message:', message, 'channel:', channel);
+
+        store.dispatch(message);
+    }
+});
+
+setTimeout(() => {
+    pubsub.publish(newMessage({text: 'Hello world!', username: 'Bob'}));
+}, 1000);
+
+ReactDOM.render(
+<Provider store={ store }>
+    <PubSubContext.Provider value={{ pubsub }}>
+      <App />   
+    </PubSubContext.Provider>
+</Provider>,
+    document.getElementById('root')
+);
+
